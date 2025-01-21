5a98cccbdc681a5e168e20e9dd8f35069b814805
nguyenHoang32 <nguyenhuuhoang2k15@gmail.com>
2025-01-21 14:55:50 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { cronjob } from './cronjob'
+import Web3 from 'web3'
+
+dotenv.config()
+
+const DB_NAME = 'btc_restaking'
+
+mongoose.connect(process.env.MONGO_URL as string, {
+  dbName: DB_NAME,
+})
+
+export const web3 = new Web3(process.env.RPC_URL)
+
+declare global {
+  interface BigInt {
+    toJSON(): Number
+  }
+}
+
+BigInt.prototype.toJSON = function () {
+  return this.toString()
+}
+
+
