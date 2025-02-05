0e892ca9ad23a8fa844b844a54808d188e080d53
nguyenHoang32 <nguyenhuuhoang2k15@gmail.com>
2025-02-05 15:44:46 +0700

    save point use api

--- src/service/snapshot.service.ts
+++ src/service/snapshot.service.ts
@@ -1,10 +1,22 @@
 import { TYPE } from '../const'
 import snapshotSchema, { ISnapshot } from '../model/snapshot.schema'
+import axios from "axios";
+import * as crypto from "crypto";
+import * as secp256k1 from "secp256k1";
 
-export const insertSnapshot = (data: ISnapshot[]) => {
-  return snapshotSchema.insertMany(data, {
-    ordered: false,
-  })
+export async function insertSnapshot(data: ISnapshot[]) {
+  try {
+    const sigObj = secp256k1.ecdsaSign(
+      Buffer.from(
+        crypto.createHash("sha256").update(JSON.stringify(data)).digest().toString("hex"),
+        "hex"
+      ),
+      Buffer.from(process.env.SAVE_POINT_PRIVATE_KEY, "hex")
+    );
+    await axios.post(process.env.SAVE_POINT_ENDPOINT, { data, signature: Buffer.from(sigObj.signature).toString("hex") })
+  } catch (error) {
+    console.error(`save point error : ${error}`)
+  }
 }
 
 export const checkSnapshotAtDate = async (date: Date) => {
